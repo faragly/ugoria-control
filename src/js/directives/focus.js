4ee8906bcb7f51ee8454b197c4a4db442cbe38ae
faragly <faragly@mail.ru>
2015-07-13 12:57:28 +0500

    Директива focus

---
+++ src/js/directives/focus.js
@@ -0,0 +1,16 @@
+module.exports = ['$timeout', function ($timeout) {
+  return {
+    scope : {
+      trigger : '@focus'
+    },
+    link : function(scope, element) {
+      scope.$watch('trigger', function(value) {
+        if (value === "true") {
+          $timeout(function() {
+            element[0].focus();
+          });
+        }
+      });
+    }
+  };
+}];
