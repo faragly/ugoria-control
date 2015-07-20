f1133c3e2d8ef274216a9129a6f335db05cc4344
faragly <faragly@mail.ru>
2015-07-20 13:52:09 +0500

    Провайдер опций angular-модуля.

---
+++ src/js/providers/options.js
@@ -0,0 +1,20 @@
+function options() {
+  var rootUrl = 'http://phwebpro.azurewebsites.net/api/Data',
+    requestInterval = 250;
+  this.setRootUrl = function(url) {
+    rootUrl = url;
+    return this; // Fluent interface, тут к месту.
+  };     
+  this.setRequestInterval = function(interval) {
+    requestInterval = interval;
+    return this;
+  };     
+  this.$get = function() {
+    return {
+      getRequestInterval: function() { return requestInterval; },
+      getRootUrl: function() { return rootUrl; }
+    };
+  }
+}
+
+module.exports = [options];
